60adb9e47f655ca00020447980165a237e792b24
何健锋 <350864815@qq.com>
2017-10-04 22:14:15 +0800

    第一版git

---
+++ src/components/Hello1.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello1">
+    <h1>{{ msg }}</h1>
+    hahahhahaha
+    <router-link to="/Hello">Go to Hello</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello1',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App11111111111111111111111'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Hello1 from '@/components/Hello1'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello1',
+      component: Hello1
+    },
+    {
+      path: '/Hello',
+      name: 'Hello',
+      component: Hello
+    }
+
+  ]
+})
